4a16c6fb5d9700292a1e095eb4da993f01639a2c
flat1ander <thematthewlane@gmail.com>
2023-03-04 09:27:07 -0500

    Working on streamlining  my code for populating the question and answer text with a function rather than a large if statement

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Declaring variables for all of the control elements:
+// Declaring variables for all of the control elements and photos:
 const answerA = document.querySelector('#answer-a')
 const answerB = document.querySelector('#answer-b')
 const answerC = document.querySelector('#answer-c')
@@ -113,7 +113,8 @@ let triviaQuestions = [
             {b: 'Scott Bakula', answer: false},
             {c: 'Peter Faulk', answer: false},
             {d: 'David Hasselhoff', answer: false},
-        ]
+        ],
+        img: "./images/television.png"
     }]
 
 // Declaring variables for the current question and the score:
@@ -167,6 +168,37 @@ function gameStart() {
 // Invoke the gameStart function
 gameStart();
 
+// Trying to streamline the large if statement comented out below:
+
+        function renderQuestion (currentQuestion) {
+            if (currentQuestion <= 9){    
+            questionText.innerHTML = currentQuestion.question;
+            answerA.innerHTML = currentQuestion.answers[0].a;
+            answerB.innerHTML = currentQuestion.answers[1].b;
+            answerC.innerHTML = currentQuestion.answers[2].c;
+            answerD.innerHTML = currentQuestion.answers[3].d;
+            nextQuestion.classList.add('hide')
+            gameStartBtn.classList.add('hide')
+            }
+            else {
+                resultDisplay.classList.remove('hide')
+                resultDisplay.innerHTML = "You scored:"
+                resultDisplay.style.color = "#00008B"
+                answerButtons.classList.add('hide');
+                nextQuestion.classList.add('hide')
+                yourScore.classList.add('hide')
+                restart.classList.remove('hide')
+                if (playerPoints >= 7) 
+                {questionText.innerHTML = "You win! Great Job!"
+                trophy.classList.remove('hide')
+            }
+                    else{questionText.innerHTML = "You didn't make the grade. Better luck next time.."
+                    facepalm.classList.remove('hide')
+                }
+            }return
+        }
+
+        
 
 // Next Question function:
 function nextQuestionFunc () {
@@ -178,118 +210,123 @@ function nextQuestionFunc () {
     resultDisplay.classList.add('hide')
     // Hide the next question button until an answer has been selected:
     nextQuestion.classList.add('hide')
-    // As the current question is incremented, the question text and possible answers change. We also hide the next question button until an answer has been selected:
-    if(currentQuestion === 1){
-        questionText.innerHTML = triviaQuestions[1].question;
-        answerA.innerHTML = triviaQuestions[1].answers[0].a;
-        answerB.innerHTML = triviaQuestions[1].answers[1].b;
-        answerC.innerHTML = triviaQuestions[1].answers[2].c;
-        answerD.innerHTML = triviaQuestions[1].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        musicNote.classList.add('hide')
-        camera.classList.remove('hide')
-    }else if(currentQuestion === 2){
-        questionText.innerHTML = triviaQuestions[2].question;
-        answerA.innerHTML = triviaQuestions[2].answers[0].a;
-        answerB.innerHTML = triviaQuestions[2].answers[1].b;
-        answerC.innerHTML = triviaQuestions[2].answers[2].c;
-        answerD.innerHTML = triviaQuestions[2].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        camera.classList.add('hide')
-        basketball.classList.remove('hide')
-    }else if(currentQuestion === 3){
-        questionText.innerHTML = triviaQuestions[3].question;
-        answerA.innerHTML = triviaQuestions[3].answers[0].a;
-        answerB.innerHTML = triviaQuestions[3].answers[1].b;
-        answerC.innerHTML = triviaQuestions[3].answers[2].c;
-        answerD.innerHTML = triviaQuestions[3].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        books.classList.remove('hide')
-        basketball.classList.add('hide')
-    }else if(currentQuestion === 4){
-        questionText.innerHTML = triviaQuestions[4].question;
-        answerA.innerHTML = triviaQuestions[4].answers[0].a;
-        answerB.innerHTML = triviaQuestions[4].answers[1].b;
-        answerC.innerHTML = triviaQuestions[4].answers[2].c;
-        answerD.innerHTML = triviaQuestions[4].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        books.classList.add('hide')
-        camera.classList.remove('hide')
-    }else if(currentQuestion === 5){
-        questionText.innerHTML = triviaQuestions[5].question;
-        answerA.innerHTML = triviaQuestions[5].answers[0].a;
-        answerB.innerHTML = triviaQuestions[5].answers[1].b;
-        answerC.innerHTML = triviaQuestions[5].answers[2].c;
-        answerD.innerHTML = triviaQuestions[5].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        camera.classList.add('hide')
-        musicNote.classList.remove('hide')
-    }else if(currentQuestion === 6){
-        questionText.innerHTML = triviaQuestions[6].question;
-        answerA.innerHTML = triviaQuestions[6].answers[0].a;
-        answerB.innerHTML = triviaQuestions[6].answers[1].b;
-        answerC.innerHTML = triviaQuestions[6].answers[2].c;
-        answerD.innerHTML = triviaQuestions[6].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        camera.classList.remove('hide')
-        musicNote.classList.add('hide')
-    }else if(currentQuestion === 7){
-        questionText.innerHTML = triviaQuestions[7].question;
-        answerA.innerHTML = triviaQuestions[7].answers[0].a;
-        answerB.innerHTML = triviaQuestions[7].answers[1].b;
-        answerC.innerHTML = triviaQuestions[7].answers[2].c;
-        answerD.innerHTML = triviaQuestions[7].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        camera.classList.add('hide')
-        musicNote.classList.remove('hide')
-    }else if(currentQuestion === 8){
-        questionText.innerHTML = triviaQuestions[8].question;
-        answerA.innerHTML = triviaQuestions[8].answers[0].a;
-        answerB.innerHTML = triviaQuestions[8].answers[1].b;
-        answerC.innerHTML = triviaQuestions[8].answers[2].c;
-        answerD.innerHTML = triviaQuestions[8].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        car.classList.remove('hide')
-        musicNote.classList.add('hide')
-    }else if(currentQuestion === 9){
-        questionText.innerHTML = triviaQuestions[9].question;
-        answerA.innerHTML = triviaQuestions[9].answers[0].a;
-        answerB.innerHTML = triviaQuestions[9].answers[1].b;
-        answerC.innerHTML = triviaQuestions[9].answers[2].c;
-        answerD.innerHTML = triviaQuestions[9].answers[3].d;
-        nextQuestion.classList.add('hide')
-        gameStartBtn.classList.add('hide')
-        television.classList.remove('hide')
-        car.classList.add('hide')
-        // Changing next question button text to read 'Complete Game' on last question
-        nextQuestion.innerHTML = 'Complete Game'
-    // Once all ten questions have been answered, a message pops up indicating your total score and thanking you for playing. Depending on the score, you will be informed if you won or lost:
-    }else {
-        resultDisplay.classList.remove('hide')
-        resultDisplay.innerHTML = "You scored:"
-        resultDisplay.style.color = "#00008B"
-        answerButtons.classList.add('hide');
-        nextQuestion.classList.add('hide')
-        yourScore.classList.add('hide')
-        restart.classList.remove('hide')
-        television.classList.add('hide')
-        if (playerPoints >= 7) 
-        {questionText.innerHTML = "You win! Great Job!"
-        trophy.classList.remove('hide')
-    }
-            else{questionText.innerHTML = "You didn't make the grade. Better luck next time.."
-            facepalm.classList.remove('hide')
-        }
-    }
 }
+
+// Add on click event listener for the function above:
+nextQuestion.addEventListener('click', nextQuestionFunc);
+
+    // As the current question is incremented, the question text and possible answers change. We also hide the next question button until an answer has been selected. We also hide the previous image for the question category and show the new one:
+//     if(currentQuestion === 1){
+//         questionText.innerHTML = triviaQuestions[1].question;
+//         answerA.innerHTML = triviaQuestions[1].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[1].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[1].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[1].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         musicNote.classList.add('hide')
+//         camera.classList.remove('hide')
+//     }else if(currentQuestion === 2){
+//         questionText.innerHTML = triviaQuestions[2].question;
+//         answerA.innerHTML = triviaQuestions[2].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[2].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[2].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[2].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         camera.classList.add('hide')
+//         basketball.classList.remove('hide')
+//     }else if(currentQuestion === 3){
+//         questionText.innerHTML = triviaQuestions[3].question;
+//         answerA.innerHTML = triviaQuestions[3].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[3].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[3].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[3].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         books.classList.remove('hide')
+//         basketball.classList.add('hide')
+//     }else if(currentQuestion === 4){
+//         questionText.innerHTML = triviaQuestions[4].question;
+//         answerA.innerHTML = triviaQuestions[4].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[4].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[4].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[4].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         books.classList.add('hide')
+//         camera.classList.remove('hide')
+//     }else if(currentQuestion === 5){
+//         questionText.innerHTML = triviaQuestions[5].question;
+//         answerA.innerHTML = triviaQuestions[5].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[5].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[5].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[5].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         camera.classList.add('hide')
+//         musicNote.classList.remove('hide')
+//     }else if(currentQuestion === 6){
+//         questionText.innerHTML = triviaQuestions[6].question;
+//         answerA.innerHTML = triviaQuestions[6].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[6].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[6].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[6].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         camera.classList.remove('hide')
+//         musicNote.classList.add('hide')
+//     }else if(currentQuestion === 7){
+//         questionText.innerHTML = triviaQuestions[7].question;
+//         answerA.innerHTML = triviaQuestions[7].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[7].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[7].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[7].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         camera.classList.add('hide')
+//         musicNote.classList.remove('hide')
+//     }else if(currentQuestion === 8){
+//         questionText.innerHTML = triviaQuestions[8].question;
+//         answerA.innerHTML = triviaQuestions[8].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[8].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[8].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[8].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         car.classList.remove('hide')
+//         musicNote.classList.add('hide')
+//     }else if(currentQuestion === 9){
+//         questionText.innerHTML = triviaQuestions[9].question;
+//         answerA.innerHTML = triviaQuestions[9].answers[0].a;
+//         answerB.innerHTML = triviaQuestions[9].answers[1].b;
+//         answerC.innerHTML = triviaQuestions[9].answers[2].c;
+//         answerD.innerHTML = triviaQuestions[9].answers[3].d;
+//         nextQuestion.classList.add('hide')
+//         gameStartBtn.classList.add('hide')
+//         television.classList.remove('hide')
+//         car.classList.add('hide')
+//         // Changing next question button text to read 'Complete Game' on last question
+//         nextQuestion.innerHTML = 'Complete Game'
+//     // Once all ten questions have been answered, a message pops up indicating your total score and thanking you for playing. Depending on the score, you will be informed if you won or lost:
+//     }else {
+//         resultDisplay.classList.remove('hide')
+//         resultDisplay.innerHTML = "You scored:"
+//         resultDisplay.style.color = "#00008B"
+//         answerButtons.classList.add('hide');
+//         nextQuestion.classList.add('hide')
+//         yourScore.classList.add('hide')
+//         restart.classList.remove('hide')
+//         television.classList.add('hide')
+//         if (playerPoints >= 7) 
+//         {questionText.innerHTML = "You win! Great Job!"
+//         trophy.classList.remove('hide')
+//     }
+//             else{questionText.innerHTML = "You didn't make the grade. Better luck next time.."
+//             facepalm.classList.remove('hide')
+//         }
+//     }
+// }
 
 // Adding a function to determine if the correct answer was clicked and adding points to the scoreboard
 // Loop through each answer button:
@@ -375,7 +412,6 @@ function restartGame () {
 
 // Adding on-click events to restart and next question with the functions declared for each above:
 restart.addEventListener('click', restartGame);
-nextQuestion.addEventListener('click', nextQuestionFunc);
 
 // Adjusting background audio down by 75%:
 audio.volume = 0.25;
